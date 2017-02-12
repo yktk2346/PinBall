fc00367cebf952e8c06fcabcbacb7dd1ee87b9bf
ヨコタタクヤ <yktk2346@gmail.com>
2017-02-12 20:50:59 +0900

    PinBallプロジェクト　課題・発展課題の提出
    確認をよろしくお願いいたします。

--- Assets/TouchText.cs
+++ Assets/TouchText.cs
@@ -30,28 +30,37 @@ public class TouchText : MonoBehaviour {
         }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
 
-        for (int i = 0; i < Input.touchCount; i++)
-        {
+        for (int i = 0; i < Input.touchCount; i++){
             myTouch = Input.touches[i];
             pos = Input.touches[i].position.x;
         }
 
-        //pos（タッチ座標）をhalfWidth（画面の横幅の半分）と比較し、左右を認識させる
-        //タッチ座標が半分より少なかったとき（左だったとき）
-        if (pos < halfWidth && "LeftFripperTag" == tag)
-        {
-            //「 `myTouch`（自作のタッチ関連の箱）の `phase` (タッチの状態？)が 
-            //`TouchPhase.Began` (既設のタッチ機能、その中の『タッチされた状態』 )と
-            //全く同じだった場合、別の関数で計算していた `SetAngle` に `flickAngle` 
-            //などが代入されますよー 」
-            if (myTouch.phase == TouchPhase.Began)
-                SetAngle(flickAngle);
 
-            if (myTouch.phase == TouchPhase.Ended)
-                SetAngle(defaultAngle);
+
+
+        //タッチしていないのに
+        //if (myTouch.phase == TouchPhase.Began)
+        //SetAngle(flickAngle);
+        //を処理して左フリッパーが上がりっぱなしになってしまうため制御する
+        if (Input.touchCount > 0) {
+
+
+            //pos（タッチ座標）をhalfWidth（画面の横幅の半分）と比較し、左右を認識させる
+            //タッチ座標が半分より少なかったとき（左だったとき）
+            if (pos < halfWidth && "LeftFripperTag" == tag){
+
+                //「 `myTouch`（自作のタッチ関連の箱）の `phase` (タッチの状態？)が 
+                //`TouchPhase.Began` (既設のタッチ機能、その中の『タッチされた状態』 )と
+                //全く同じだった場合、別の関数で計算していた `SetAngle` に `flickAngle` 
+                //などが代入されますよー 」
+                if (myTouch.phase == TouchPhase.Began)
+                    SetAngle(flickAngle);
+
+                if (myTouch.phase == TouchPhase.Ended)
+                    SetAngle(defaultAngle);
+            }
         }
     }
 
